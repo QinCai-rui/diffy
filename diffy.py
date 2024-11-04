f1fa334c2a8d9a12bf7a508ca71c6861aaf4ea0d
QinCai-rui <140027854+QinCai-rui@users.noreply.github.com>
2024-11-04 05:15:13 +0000

    Added an UI & error handling

--- diffy.py
+++ diffy.py
@@ -1,3 +1,5 @@
+from sys import exit
+
 def diffy_squares(a, b, c, d):
     counter = 0
     while not (a == 0 and b == 0 and c == 0 and d == 0):
@@ -21,9 +23,26 @@ def find_max_score(lower_range_limit, upper_range_limit):
 
     return max_score, max_values
 
-# Set a limit for the range of initial values
-lower_range_limit = 0
-upper_range_limit = 100000
+def main():
+    print("You will be asked for the number range being used.")
+    print("Please enter a small range to start with, as this code takes a bit time to run.")
+    print("(A range of about 500 is good to start with!)")
+    
+    lower_range_limit = int(input("\nEnter the minimum number the program may use: "))
+    upper_range_limit = int(input("\nAnd now, enter the maximum number the program may use: "))
+    
+    max_score, max_values = find_max_score(lower_range_limit, upper_range_limit + 1)
+    print(f"Max score using numbers {lower_range_limit} to {upper_range_limit}: {max_score} with values: {max_values}")
 
-max_score, max_values = find_max_score(lower_range_limit, upper_range_limit + 1)
-print(f"Max score using numbers {lower_range_limit} to {upper_range_limit}: {max_score} with values: {max_values}")
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\nYou pressed CTRL+C. EXITING...")
+        exit(0)
+    except ValueError as ve:
+        print("\nInvalid input. Make sure you are entering *INTEGERS*!!")
+        print(f"ERROR: {ve}")
+    except Exception as e:
+        print("\nAn unknown error occurred")
+        print(f"ERROR: {e}")
